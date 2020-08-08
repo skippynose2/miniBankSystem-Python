60352d39819a3ba95bfd09c0524a76ec80848d9c
Akhil <devarapalli148@gmail.com>
2020-08-08 17:33:22 -0400

    Input Boxes

---
+++ appJar.py
@@ -0,0 +1,21 @@
+from appJar import gui
+
+app = gui()
+
+#Input Feilds
+app.addLabelEntry("User Name")
+app.addLabelSecretEntry("Password")
+
+def LoginBtn(button):
+    if button == "Cancel":
+        app.stop()
+
+    else:
+        username = app.getEntry("User Name")
+        password = app.getEntry("Password")
+        print(username)
+        print(password)
+
+
+app.addButtons(["Login", "Cancel"], LoginBtn)    
+app.go()
